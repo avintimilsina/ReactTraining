09c416649f31f5658cad831ab23a4d459dda082d
Avin-T <avin.timilsina.16@gmail.com>
2022-11-12 09:17:59 +0545

    Deleted tic tac toe files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import "./App.css";
-import Game from "./components/Game";
-
 function App() {
-  return (
-    <div className='App'>
-      <Game />
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
