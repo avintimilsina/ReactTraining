e124bcd06356eeedd75915f3e237956daf26ceca
Avin-T <avin.timilsina.16@gmail.com>
2022-11-26 09:37:50 +0545

    Used React Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-function App() {
-  const [notifications, setNotifications] = useState(0);
-  useEffect(() => {
-    if (notifications > 0) {
-      document.title = "You have " + notifications + " notifications";
-    }
-  });
-
-  function sendNotifications() {
-    setNotifications(notifications + 1);
-  }
+import Weather from "./components/Weather";
+import { Outlet, Link, Routes, Route } from "react-router-dom";
+export default function App() {
   return (
-    <div className='App'>
-      <button onClick={sendNotifications}>Send Notification</button>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/weather'>Weather</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />}>
+          <Route path='/posts' element={<Post />} />
+          <Route path='/weather' element={<Weather />} />
+        </Route>
+        <Route path='/weather' element={<Weather />} />
+      </Routes>
     </div>
   );
 }
+function Home() {
+  return (
+    <>
+      <div>Header</div>
+      <div>Sidebar</div>
+      <Outlet />
+      <div>Footer</div>
+    </>
+  );
+}
 
-export default App;
+function Post() {
+  return <div>My posts</div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
-import Weather from './components/Weather'
 import './App.css';
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className="App">
-      <Weather />
-    </div>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
   </React.StrictMode>
 );
 reportWebVitals();
